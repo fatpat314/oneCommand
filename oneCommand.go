f712e6bf86d1f47b72eb2c46d492af6399153bec
Patrick <w.patrick.kelly@gmail.com>
2020-07-03 00:11:28 -0700

    automated

--- oneCommand.go
+++ oneCommand.go
@@ -4,36 +4,52 @@ import (
 	"bytes"
 	// "context"
 	// "encoding/json"
-    "bufio"
+    // "bufio"
 	"fmt"
 	// "io"
 	// "io/ioutil"
 	"log"
-	"os"
+	// "os"
 	"os/exec"
 	"strings"
 	// "time"
 )
 
 func ExampleCmd_Run() {
-    cmd := exec.Command("git", "init")
+
+    cmd := exec.Command("git", "add", ".")
 	cmd.Stdin = strings.NewReader("some input")
 	var out bytes.Buffer
 	cmd.Stdout = &out
+
+    cmd2 := exec.Command("git", "commit", "-m", "automated")
+
+    var out2 bytes.Buffer
+    cmd2.Stdout = &out2
+
+
 	err := cmd.Run()
+    err2 := cmd2.Run()
+
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
+    if err2 != nil {
+        log.Fatal(err2)
+    }
 	fmt.Printf(out.String())
+    fmt.Printf(out2.String())
 }
 
 func main(){
 
     ExampleCmd_Run()
 
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Enter your city: ")
-    city, _ := reader.ReadString('\n')
-    fmt.Print("You live in " + city)
+    // reader := bufio.NewReader(os.Stdin)
+    // fmt.Print("Enter your city: ")
+    // city, _ := reader.ReadString('\n')
+    // fmt.Print("You live in " + city)
 
 }
